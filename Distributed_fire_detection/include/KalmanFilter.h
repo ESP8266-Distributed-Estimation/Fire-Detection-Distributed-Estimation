6fed69bbdb220404a80dfeb58d7560231f43baaf
null/0x <146344604+NikhiL34343@users.noreply.github.com>
2026-04-19 15:27:58 +0530

    Update KalmanFilter.h

--- Distributed_fire_detection/include/KalmanFilter.h
+++ Distributed_fire_detection/include/KalmanFilter.h
@@ -8,6 +8,7 @@ private:
     float _current_estimate; // State (x)
     float _last_estimate;    // Previous state
     float _last_innovation;  // Difference between measurement and prediction
+    float _bias; // Bias
 
 public:
     KalmanFilter(float mea_e, float est_e, float q, float initial_value);
@@ -18,4 +19,5 @@ public:
     void setEstimateError(float est_e);
     void setProcessNoise(float q);
     float getConfidence() const; // Returns a 0-100% confidence heuristic based on variance
+    void setBias(float bias);
 };
